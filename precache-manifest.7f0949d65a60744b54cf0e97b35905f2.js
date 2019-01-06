12c2b56fe8d0de4f7e21089d7b9ac97862b1f8a3
Roman <x0kill@ya.ru>
2019-01-06 04:08:26 +0300

    Updates

--- precache-manifest.7f0949d65a60744b54cf0e97b35905f2.js
+++ precache-manifest.7f0949d65a60744b54cf0e97b35905f2.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/data-grabber/static/js/runtime~main.a9fbfa2a.js"
   },
   {
-    "revision": "8c27d01a35f1ca116b6d",
-    "url": "/data-grabber/static/js/main.8c27d01a.chunk.js"
+    "revision": "efbd99941a24bed99786",
+    "url": "/data-grabber/static/js/main.efbd9994.chunk.js"
   },
   {
     "revision": "5676a8c25768f744c9a4",
     "url": "/data-grabber/static/js/1.5676a8c2.chunk.js"
   },
   {
-    "revision": "119e6dc09e1377d023dd5be9ed29cded",
+    "revision": "969adedfa4465ffa88fe83c2515694c4",
     "url": "/data-grabber/index.html"
   }
 ];

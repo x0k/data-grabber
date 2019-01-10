2cc3f3d6a7dd28b11003e0fd37d3e9093ee2626e
Roman <x0kill@ya.ru>
2019-01-11 01:30:35 +0300

    Updates

--- precache-manifest.aa4d0b606a84253f32799a84df1c44c7.js
+++ precache-manifest.aa4d0b606a84253f32799a84df1c44c7.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/data-grabber/static/js/runtime~main.a9fbfa2a.js"
   },
   {
-    "revision": "4c0a34a66e5fb2112d62",
-    "url": "/data-grabber/static/js/main.4c0a34a6.chunk.js"
+    "revision": "f8deacb4d55e25c4578c",
+    "url": "/data-grabber/static/js/main.f8deacb4.chunk.js"
   },
   {
     "revision": "8d26f76ec214751990d1",
     "url": "/data-grabber/static/js/1.8d26f76e.chunk.js"
   },
   {
-    "revision": "251be82801c858aba218612df92bfc0e",
+    "revision": "46e38ff566ce98676984b70b6d17c2f7",
     "url": "/data-grabber/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/data-grabber/precache-manifest.3677e93758fd28e2f8df7858c8278986.js"
+  "/data-grabber/precache-manifest.aa4d0b606a84253f32799a84df1c44c7.js"
 );
 
 workbox.clientsClaim();
